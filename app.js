02c76678ecc19dff3304eba66a824f634b482d30
kaihengann <annkaiheng@gmail.com>
2019-06-05 14:30:48 +0800

    Refactor Item component and fruitItems variable

--- app.js
+++ app.js
@@ -3,40 +3,41 @@ const fruits = ["apple", "oranges", "mangos", "watermelon"];
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' }
+    this.state = { value: "" };
   }
 
   // On user input, set form value to SearchForm state
   handleChange = e => {
     this.setState({ value: e.target.value });
-  }
+  };
 
   // Returns array of filtered fruits after user input
   filterFruits = () => {
-    return fruits.filter((fruit) => fruit.includes(this.state.value))
-  }
+    return fruits.filter(fruit => fruit.includes(this.state.value));
+  };
 
   render() {
-    const fruitItems = this.filterFruits().map((fruitName) => {
-      return <Item fruitName={fruitName} />
-    });
+    const fruitItems = this.filterFruits().map(fruitName => (
+      <Item fruitName={fruitName} />
+    ));
 
     return (
-      <div id='container'>
-        <input type='text' onChange={this.handleChange} value={this.state.value} placeholder='Enter a fruit' />
-        <ul>
-          {fruitItems}
-        </ul>
+      <div id="container">
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.value}
+          placeholder="Enter a fruit"
+        />
+        <ul>{fruitItems}</ul>
       </div>
-    )
+    );
   }
 }
 
-function Item({fruitName}) {
-  return <li>{fruitName}</li>
-}
+const Item = ({ fruitName }) => <li>{fruitName}</li>;
 
-const element = <SearchForm />
-const container = document.getElementById('app')
+const element = <SearchForm />;
+const container = document.getElementById("app");
 
-ReactDOM.render(element, container)
+ReactDOM.render(element, container);
